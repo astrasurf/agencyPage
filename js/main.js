b7ebee8eb9b974111b1f5bbae98f7970320fdbea
Seb <free.sebastian@gmail.com>
2021-07-18 12:18:22 -0600

    SurferDude

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", init)
 
 function init() {
     console.log("Page Initiated")
-    gsap.set([sideMenu], {x:-267})
+    gsap.set([sideMenu], {x:-472})
     document.getElementById("hamburgerHolder").addEventListener("click", hamburgerOpen);
 }
 
@@ -10,7 +10,7 @@ function hamburgerOpen() {
     console.log("Open up sideMenu")
     document.getElementById("hamburgerHolder").removeEventListener("click", hamburgerOpen);
     document.getElementById("hamburgerHolder").addEventListener("click", hamburgerClose);
-    gsap.to([sideMenu], {duration: 0.6, x:0, ease:"power4.out"})
+    gsap.to([sideMenu], {duration: 0.6, x:40, ease:"power4.out"})
     gsap.to([hamLine1], {duration:0.6, rotation:45})
     gsap.to([hamLine2], {duration:0.6, opacity:0})
     gsap.to([hamLine3], {duration:0.6, y:-23, rotation:135})
@@ -21,7 +21,7 @@ function hamburgerClose() {
     console.log("Close sideMenu")
     document.getElementById("hamburgerHolder").removeEventListener("click", hamburgerClose);
     document.getElementById("hamburgerHolder").addEventListener("click", hamburgerOpen);
-    gsap.to([sideMenu], {duration: 0.6, x:-267, ease:"power4.out"})
+    gsap.to([sideMenu], {duration: 0.6, x:-472, ease:"power4.out"})
     gsap.to([hamLine1], {duration:0.6, rotation:0})
     gsap.to([hamLine2], {duration:0.6, opacity:1})
     gsap.to([hamLine3], {duration:0.6, y:0, rotation:0})
